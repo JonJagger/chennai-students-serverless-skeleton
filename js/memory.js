140f2adbff459950e2d254fd8c6d08930cffdaa5
Jon Jagger <jon@jaggersoft.com>
2017-08-04 10:14:58 +0530

    initial commit

---
+++ js/memory.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+  let names = [
+    'Akhil',
+    'Akshaya',
+    'Arun',
+    'ArunKumar',
+    'Ashwin',
+    'Krishna',
+    'Deekshita',
+    'Divya',
+    'Francis',
+    'Gayathri',
+    'Heamanth',
+    'Madhu',
+    'Manisha',
+    'Karthick',
+    'Mihir',
+    'Murali',
+    'Neeharika',
+    'Prakash',
+    'Prakhyat',
+    'Sanjana',
+    'Sankara',
+    'Sariha',
+    'Shravan',
+    'Sindhura',
+    'Srikar',
+    'Surya',
+    'Tharun',
+    'Uma',
+    'Vamshi',
+    'Veena',
+    'Vignesh',
+    'Vinay'
+  ];
+
+  alert("Hello world");
+
+});
