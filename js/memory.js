967f6c8e6116ad4eed27fe130b2d5c843f77386f
Jon Jagger <jon@jaggersoft.com>
2017-08-06 15:53:06 +0530

    refactoring

--- js/memory.js
+++ js/memory.js
@@ -7,35 +7,12 @@ $(document).ready(function() {
   ];
 
   const namesJPG = [
-    'Akhil',
-    'Akshaya',
-    'Arun',
-    'ArunKumar',
-    'Ashwin',
-    'Krishna',
-    'Divya',
-    'Francis',
-    'Gayathri',
-    'Heamanth',
-    'Madhu',
-    'Manisha',
-    'Karthick',
-    'Mihir',
-    'Murali',
-    'Neeharika',
-    'Prakash',
-    'Prakhyat',
-    'Sanjana',
-    'Sankara',
-    'Sariha',
-    'Shravan',
-    'Sindhura',
-    'Tharun',
-    'Uma',
-    'Vamshi',
-    'Veena',
-    'Vignesh',
-    'Vinay'
+    'Akhil', 'Akshaya', 'Arun', 'ArunKumar', 'Ashwin',
+    'Krishna', 'Divya', 'Francis', 'Gayathri', 'Heamanth',
+    'Madhu', 'Manisha', 'Karthick', 'Mihir', 'Murali',
+    'Neeharika', 'Prakash', 'Prakhyat', 'Sanjana', 'Sankara',
+    'Sariha', 'Shravan', 'Sindhura', 'Tharun', 'Uma',
+    'Vamshi', 'Veena', 'Vignesh', 'Vinay'
   ];
 
   alert("Hello world");
